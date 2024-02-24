b86c0d3c318d6900c2e75ae15e162cadd3cfce89
Mark Homchik <markhomchik4@gmail.com>
2024-02-24 09:54:21 -0600

    all concats

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-let number
-let upperCase
-let lowerCase
-let character
-let choice
+var number, upperCase, lowerCase, character
+
+var choice
 
 //All possible characters
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-character = ['!', '@', '#', '$', '%',
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+characters = ['!', '@', '#', '$', '%',
     '^', '&', '*', '(', ')', '_', '-', '=',
     '+', '~', '`', '{', '}', '[', ']', '|',
     '\\', ':', ';', '"', "'", ',', '<', '>', '/', '?'];
-letters['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var toUpper = function(x){
+    return x.toUpperCase();
+};
+
+var letters2 = letters(toUpper);
 // Write password to the #password input
 function writePassword() {
     //Asks User Input
-    let passwordlength = prompt('Enter the desired length of your password:');
+    var passwordlength = prompt('Enter the desired length of your password:');
     //Error if criteria doesn't fit
     if (passwordlength === null || isNaN(passwordlength) || passwordlength < 8 || passwordlength > 128) {
         alert('Error. Password length must be at least 8 characters and no more than 128.');
@@ -34,70 +38,70 @@ function writePassword() {
     }
     //For if all criteria gets chosen
     else if (number && upperCase && lowerCase && character) {
-        choice
+        choice = numbers.concat(characters, letters, letters2)
     }
     //For if only 3 criteria get chosen
     else if (number && upperCase && lowerCase) {
-        choice
+        choice = numbers.concat(letters2, letters);
     }
 
     else if (number && upperCase && character) {
-        choice
+        choice = numbers.concat(letters2, characters);
     }
 
     else if (upperCase && lowerCase && character) {
-        choice
+        choice = letters2.concat(letters, characters);
     }
 
     else if (number && lowercase && character) {
-        choice
+        choice = numbers.concat(letters, characters);
     }
     //For if only 2 criteria get chosen
     else if (number && upperCase) {
-        choice
+        choice = numbers.concat(letters2);
     }
 
     else if (number && lowerCase) {
-        choice
+        choice = numbers.concat(letters);
     }
 
     else if (number && character) {
-        choice
+        choice = numbers.concat(characters);
     }
 
     else if (upperCase && lowerCase) {
-        choice
+        choice = letters2.concat(letters)
     }
 
     else if (upperCase && character) {
-        choice
+        choice = letters2.concat(characters);
     }
 
     else if (character && lowerCase) {
-        choice
+        choice = characters.concat(letters)
     }
     //For if only 1 criteria get chosen
     else if (number) {
-        choice
+        choice = numbers
     }
 
     else if (upperCase) {
-        choice
+        choice = letters2
     }
 
     else if (lowerCase) {
-        choice
+        choice = letters
     }
 
     else if (character) {
-        choice
+        choice = characters
     }
 
 
 
 
 
-    var password = generatePassword();
+    var password = generatePassword(passwordlength);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
